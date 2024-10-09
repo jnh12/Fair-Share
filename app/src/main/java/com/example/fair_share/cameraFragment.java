0ca3e45174d862c080dc080006d1d092d59710ce
jnh12 <jnh12@mail.aub.edu>
2024-10-09 20:05:57 +0300

    push

--- app/src/main/java/com/example/fair_share/cameraFragment.java
+++ app/src/main/java/com/example/fair_share/cameraFragment.java
@@ -11,6 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.camera.core.CameraSelector;
@@ -24,31 +28,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class cameraFragment extends Fragment {
     private PreviewView previewView;
     private ImageCapture imageCapture;
     private static final int REQUEST_CAMERA_PERMISSION = 1001;
-    private OkHttpClient client = new OkHttpClient();
 
 
     @Nullable
@@ -100,22 +93,8 @@ public class cameraFragment extends Fragment {
                 public void onCaptureSuccess(@NonNull ImageProxy image) {
                     @SuppressLint("UnsafeExperimentalUsageError")
                     Bitmap bitmap = imageProxyToBitmap(image);
-
-                    File imageFile = saveBitmapToFile(bitmap);
-
-                    if (imageFile != null) {
-                        uploadReceiptImage(imageFile, new OCRCallback() {
-                            @Override
-                            public void onSuccess(String result) {
-                                sendTextToBackend(result);
-                            }
-
-                            @Override
-                            public void onError(String error) {
-                                System.err.println("OCR Error: " + error);
-                            }
-                        });
-                    }
+                    InputImage inputImage = InputImage.fromBitmap(bitmap, image.getImageInfo().getRotationDegrees());
+                    extractTextFromImage(inputImage);
 
                     byte[] imageData = bitmapToByteArray(bitmap);
                     sendImageToBackend(imageData);
@@ -131,10 +110,35 @@ public class cameraFragment extends Fragment {
         }
     }
 
+    private void sendParsedTextToBackend(String resultText) {
+        new Thread(() -> {
+            try {
+                URL url = new URL("http://10.0.2.2:8080/api/ocr/parseText");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
+
+                String jsonInputString = "\"" + resultText + "\"";
+                try (OutputStream os = conn.getOutputStream()) {
+                    byte[] input = jsonInputString.getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                }
+
+                int code = conn.getResponseCode();
+                Log.d("Backend TEXT Response", "Code: " + code);
+
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     private void sendTextToBackend(String resultText) {
         new Thread(() -> {
             try {
-                URL url = new URL("http://192.168.100.5:8080/api/ocr/saveText");
+                URL url = new URL("http://10.0.2.2:8080/api/ocr/saveText");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Content-Type", "application/json");
@@ -159,7 +163,7 @@ public class cameraFragment extends Fragment {
     private void sendImageToBackend(byte[] resultImage) {
         new Thread(() -> {
             try {
-                URL url = new URL("http://192.168.100.5:8080/api/ocr/saveImage");
+                URL url = new URL("http://10.0.2.2:8080/api/ocr/saveImage");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Content-Type", "application/octet-stream"); // Set to octet-stream for binary data
@@ -187,54 +191,6 @@ public class cameraFragment extends Fragment {
         return stream.toByteArray();
     }
 
-    private File saveBitmapToFile(Bitmap bitmap) {
-        File imageFile = new File(requireContext().getExternalFilesDir(null), "captured_image.jpg");
-        try (FileOutputStream out = new FileOutputStream(imageFile)) {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // Save the bitmap as a JPEG file
-            return imageFile;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public void uploadReceiptImage(File imageFile, OCRCallback callback) {
-        MediaType mediaType = MediaType.parse("image/jpeg");
-
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("api_key", "TEST") // Your API key
-                .addFormDataPart("recognizer", "auto") // Recognizer type
-                .addFormDataPart("ref_no", "ocr_java_123") // Reference number (optional)
-                .addFormDataPart("file", imageFile.getName(), RequestBody.create(mediaType, imageFile))
-                .build();
-
-        Request request = new Request.Builder()
-                .url("https://ocr.asprise.com/api/v1/receipt")
-                .post(requestBody)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                // Call the callback with an error message
-                callback.onError(e.getMessage());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    String responseBody = response.body().string();
-                    // Call the callback with the result
-                    callback.onSuccess(responseBody);
-                } else {
-                    // Call the callback with an error message
-                    callback.onError("Request failed: " + response.message());
-                }
-            }
-        });
-    }
-
     private Bitmap imageProxyToBitmap(ImageProxy image) {
         ByteBuffer buffer = image.getPlanes()[0].getBuffer();
         byte[] bytes = new byte[buffer.remaining()];
@@ -242,17 +198,65 @@ public class cameraFragment extends Fragment {
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 
-    //    private void extractTextFromImage(InputImage image) {
-//        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-//        recognizer.process(image)
-//                .addOnSuccessListener(result -> {
-//                    String resultText = result.getText();
-//                    Log.d("OCR Result", resultText);
-//                    sendTextToBackend(resultText);
-//
-//                })
-//                .addOnFailureListener(e -> {
-//                    e.printStackTrace();
-//                });
+    private void extractTextFromImage(InputImage image) {
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+                .addOnSuccessListener(result -> {
+                    String resultText = result.getText();
+                    Log.d("OCR Result", resultText);
+                    sendTextToBackend(resultText);
+                    sendParsedTextToBackend(resultText);
+
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+    }
+
+    //  private File saveBitmapToFile(Bitmap bitmap) {
+//        File imageFile = new File(requireContext().getExternalFilesDir(null), "captured_image.jpg");
+//        try (FileOutputStream out = new FileOutputStream(imageFile)) {
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // Save the bitmap as a JPEG file
+//            return imageFile;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
 //    }
+   // public void uploadReceiptImage(File imageFile, OCRCallback callback) {
+//        MediaType mediaType = MediaType.parse("image/jpeg");
+//
+//        RequestBody requestBody = new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("api_key", "TEST") // Your API key
+//                .addFormDataPart("recognizer", "auto") // Recognizer type
+//                .addFormDataPart("ref_no", "ocr_java_123") // Reference number (optional)
+//                .addFormDataPart("file", imageFile.getName(), RequestBody.create(mediaType, imageFile))
+//                .build();
+//
+//        Request request = new Request.Builder()
+//                .url("https://ocr.asprise.com/api/v1/receipt")
+//                .post(requestBody)
+//                .build();
+//
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                // Call the callback with an error message
+//                callback.onError(e.getMessage());
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                if (response.isSuccessful()) {
+//                    String responseBody = response.body().string();
+//                    // Call the callback with the result
+//                    callback.onSuccess(responseBody);
+//                } else {
+//                    // Call the callback with an error message
+//                    callback.onError("Request failed: " + response.message());
+//                }
+//            }
+//        });
+//
 }
